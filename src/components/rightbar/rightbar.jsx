e521ce5d64f0055078a3963f4d84ccf5eff597c8
Gokul Krishna <gokulkrishnatu11@gmail.com>
2022-12-16 11:00:25 +0600

    added component rightbar and created elements

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -4,7 +4,99 @@ import "./rightbar.css"
 function rightbar() {
   return (
     <div className='rightbar'>
-      rightbar
+      <div className='rightbarWrapper'>
+        <div className='birthdayContainer'>
+          <img className='birthdayImg' src="assets/gift.png" alt="" />
+        <span className='birthdayText'>
+          <b>Neymar jr</b> and <b>3 other friends</b> have birthday today
+        </span>
+        </div>
+      <img className='rightbarAd' src="assets/ad.png" alt="" />
+      <h4 className="rightbarTitle"> <b>Online Friends</b> </h4>
+      <ul className="rightbarFriendList">
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+
+
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+
+
+
+
+        <li className="rightbarFriends">
+          <div className="rightbarProfileImgContainer">
+            <img className="rightbarProfileImg"
+             src="assets/person/neymar.jpg" 
+             alt="" />
+             <span className="rightbarOnline"git status></span>
+          </div>
+          <span className='rightbarUsername'>Neymar jr</span>
+        </li>
+
+
+
+
+
+
+
+
+        
+      </ul>
+      </div>
+    
     </div>
   )
 }
